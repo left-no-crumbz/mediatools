f55d1a839eb31918ee0a9494486bb97ec22e6408
Dzone <133969306+left-no-crumbz@users.noreply.github.com>
2025-04-20 10:31:30 +0800

    feat: Refactor code to use the strategy pattern for rgba img

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-import time
 from io import BytesIO
+from time import perf_counter
 from types import ModuleType
-from typing import Literal
+from typing import Literal, cast
 
 import numpy as np
 import onnxruntime as ort
@@ -15,9 +15,9 @@ original_get_module_paths = local_sources_watcher.get_module_paths
 
 def profiler(func):
     def wrapper(*args, **kwargs):
-        start = time.perf_counter()
+        start = perf_counter()
         result = func(*args, **kwargs)
-        elapsed = time.perf_counter() - start
+        elapsed = perf_counter() - start
         print(f"{func.__name__} took {elapsed:.4f} seconds to execute")
         return result
 
@@ -36,15 +36,15 @@ local_sources_watcher.get_module_paths = patched_get_module_paths
 
 class Strategy:
     def __init__(
-        self,
+        _self,
         img_bytes: bytes,
         target_size: tuple[int, int] = (256, 256),
     ) -> None:
-        self._img_bytes = img_bytes
-        self._target_size = target_size
+        _self._img_bytes = img_bytes
+        _self._target_size = target_size
 
     def get_resample_method(
-        self, size1: tuple[int, int], size2: tuple[int, int]
+        _self, size1: tuple[int, int], size2: tuple[int, int]
     ) -> Literal[Image.Resampling.LANCZOS, Image.Resampling.BILINEAR]:
         if size1[0] > size2[0] or size1[1] > size2[1]:
             resample_method = Image.Resampling.LANCZOS
@@ -54,35 +54,40 @@ class Strategy:
         return resample_method
 
     # to be abstracted
-    def preprocesss(self):
+    def preprocess(_self):
         pass
 
     # to be abstracted
-    def postprocess(self):
+    def postprocess(_self):
         pass
 
 
 class RGBStrategy(Strategy):
-    def preprocesss(self) -> tuple[np.ndarray, tuple[int, int]]:
-        _img = Image.open(BytesIO(self._img_bytes))
+    @st.cache_data
+    def preprocess(_self) -> tuple[np.ndarray, tuple[int, int]]:
+        _img = Image.open(BytesIO(_self._img_bytes))
         orig_size = _img.size
 
-        resample_method = self.get_resample_method(self._target_size, orig_size)
+        resample_method = _self.get_resample_method(_self._target_size, orig_size)
 
-        img = _img.resize(self._target_size, resample_method)
+        img = _img.resize(_self._target_size, resample_method)
         arr = np.array(img).astype(np.float32)
         arr = arr.transpose(2, 0, 1)
         arr = arr[np.newaxis, ...] / 255.0
         return arr, orig_size
 
+    @st.cache_data
     def postprocess(
-        self, model_output: np.ndarray, orig_size: tuple[int, int], do_retain_size: bool
+        _self,
+        model_output: np.ndarray,
+        orig_size: tuple[int, int],
+        do_retain_size: bool = False,
     ) -> Image.Image:
         arr = model_output.squeeze().transpose(1, 2, 0)
         arr = np.clip(arr * 255.0, 0, 255).astype(np.uint8, copy=False)
         out_size = (arr.shape[1], arr.shape[0])
 
-        resample_method = self.get_resample_method(orig_size, out_size)
+        resample_method = _self.get_resample_method(orig_size, out_size)
 
         if do_retain_size:
             img = Image.fromarray(arr)
@@ -93,14 +98,15 @@ class RGBStrategy(Strategy):
 
 
 class RGBAStrategy(Strategy):
-    def preprocess(self) -> tuple[np.ndarray, np.ndarray, tuple[int, int]]:
-        _img = Image.open(BytesIO(self._img_bytes))
+    @st.cache_data
+    def preprocess(_self) -> tuple[np.ndarray, np.ndarray, tuple[int, int]]:
+        _img = Image.open(BytesIO(_self._img_bytes))
         orig_size = _img.size
 
-        resample_method = self.get_resample_method(self._target_size, orig_size)
+        resample_method = _self.get_resample_method(_self._target_size, orig_size)
 
-        rgb_img = _img.convert("RGB").resize(self._target_size, resample_method)
-        alpha = _img.split()[-1].resize(self._target_size, resample_method)
+        rgb_img = _img.convert("RGB").resize(_self._target_size, resample_method)
+        alpha = _img.split()[-1].resize(_self._target_size, resample_method)
         arr = (
             np.array(rgb_img).astype(np.float32).transpose(2, 0, 1)[np.newaxis, ...]
             / 255.0
@@ -110,24 +116,25 @@ class RGBAStrategy(Strategy):
         )
         return arr, alpha_arr, orig_size
 
+    @st.cache_data
     def postprocess(
-        self,
+        _self,
         model_output: np.ndarray,
         alpha_out: np.ndarray,
         orig_size: tuple[int, int],
-        do_retain_size: bool,
+        do_retain_size: bool = False,
     ) -> Image.Image:
         arr = model_output.squeeze().transpose(1, 2, 0)
         arr = np.clip(arr * 255.0, 0, 255).astype(np.uint8)
 
         out_size = (arr.shape[1], arr.shape[0])
 
-        resample_method = self.get_resample_method(orig_size, out_size)
+        resample_method = _self.get_resample_method(orig_size, out_size)
 
         if do_retain_size:
-            img = Image.fromarray(arr).resize(orig_size, resample_method)
+            img = Image.fromarray(arr)
 
-        img = Image.fromarray(arr)
+        img = Image.fromarray(arr).resize(orig_size, resample_method)
         alpha = alpha_out.squeeze()
         alpha = np.clip(alpha * 255.0, 0, 255).astype(np.uint8)
 
@@ -263,70 +270,93 @@ def main():
         accept_multiple_files=False,
     )
 
+    do_retain_size = st.checkbox("Retain original size")
+
     device, model = load_model()
 
     st.info(f"Using device: {device}")
 
     if uploaded_img:
         img_bytes = uploaded_img.read()
-        try:
-            col1, col2 = st.columns(2)
-            original_img = Image.open(BytesIO(img_bytes))
-            input_name = model.get_inputs()[0].name
+        col1, col2 = st.columns(2)
+        original_img = Image.open(BytesIO(img_bytes))
+        input_name = model.get_inputs()[0].name
 
-            with col1:
-                st.header("Original Image")
-                st.image(original_img)
+        with col1:
+            st.header("Original Image")
+            st.image(original_img)
 
-            info_placeholder = st.empty()
-            info_placeholder.info("Preparing image for upscaling...")
+        info_placeholder = st.empty()
+        info_placeholder.info("Preparing image for upscaling...")
 
-            with st.spinner("Upscaling image..."):
-                if original_img.mode == "RGBA":
-                    info_placeholder.info("⚙ Preprocessing the image...")
-                    input_arr, alpha_arr, orig_size = preprocess_rgba(img_bytes)  # type: ignore
-                    info_placeholder.info("🏃‍♀️ Running the model...")
-                    output = model.run(None, {input_name: input_arr})[0]
+        with st.spinner("Upscaling image..."):
+            strategy = (
+                RGBStrategy(img_bytes)
+                if original_img.mode != "RGBA"
+                else RGBAStrategy(img_bytes)
+            )
 
-                    print(f"Output: {type(output)}")
+            if original_img.mode == "RGBA":
+                strategy = cast(RGBAStrategy, strategy)
 
-                    alpha_out = alpha_arr
-                    info_placeholder.info("⚙ Postprocessing the image...")
-                    sr_img = postprocess_rgba(output, alpha_out, orig_size)
-                    info_placeholder.info("🏁 Finished!")
+                info_placeholder.info("⚙ Preprocessing the image...")
+                # input_arr, alpha_arr, orig_size = preprocess_rgba(img_bytes)  # type: ignore
 
-                else:
-                    info_placeholder.info("⚙ Preprocessing the image...")
-                    input_arr, orig_size = preprocess(img_bytes)
-                    info_placeholder.info("🏃‍♀️ Running the model...")
-                    output = model.run(None, {input_name: input_arr})[0]
-
-                    print(f"Output: {type(output)}")
-
-                    info_placeholder.info("⚙ Postprocessing the image...")
-                    sr_img = postprocess(output, orig_size)
-                    info_placeholder.info("🏁 Finished!")
-
-            info_placeholder.empty()
-
-            if sr_img:
-                with col2:
-                    st.header("Upscaled Image")
-                    st.image(sr_img)
-
-                buffer = convert_img_to_bytes(sr_img)
-
-                st.download_button(
-                    label="Download image",
-                    data=buffer,
-                    file_name="sr_image.png",
-                    mime="image/png",
-                    icon=":material/download:",
-                    use_container_width=True,
+                input_arr, alpha_arr, orig_size = cast(
+                    tuple[np.ndarray, np.ndarray, tuple[int, int]],
+                    strategy.preprocess(),
                 )
-        except Exception as e:
-            print(f"ERROR: {e}")
-            st.error(f"Error processing image: {e}")
+
+                info_placeholder.info("🏃‍♀️ Running the model...")
+                output = model.run(None, {input_name: input_arr})[0]
+
+                print(f"Output: {type(output)}")
+
+                alpha_out = alpha_arr
+                info_placeholder.info("⚙ Postprocessing the image...")
+
+                sr_img = cast(
+                    Image.Image,
+                    strategy.postprocess(
+                        model_output=output,
+                        alpha_out=alpha_out,
+                        orig_size=orig_size,
+                        do_retain_size=do_retain_size,
+                    ),
+                )
+
+                # sr_img = postprocess_rgba(output, alpha_out, orig_size)
+                info_placeholder.info("🏁 Finished!")
+
+            else:
+                info_placeholder.info("⚙ Preprocessing the image...")
+                input_arr, orig_size = preprocess(img_bytes)
+                info_placeholder.info("🏃‍♀️ Running the model...")
+                output = model.run(None, {input_name: input_arr})[0]
+
+                print(f"Output: {type(output)}")
+
+                info_placeholder.info("⚙ Postprocessing the image...")
+                sr_img = postprocess(output, orig_size)
+                info_placeholder.info("🏁 Finished!")
+
+        info_placeholder.empty()
+
+        if sr_img:
+            with col2:
+                st.header("Upscaled Image")
+                st.image(sr_img)
+
+            buffer = convert_img_to_bytes(sr_img)
+
+            st.download_button(
+                label="Download image",
+                data=buffer,
+                file_name="sr_image.png",
+                mime="image/png",
+                icon=":material/download:",
+                use_container_width=True,
+            )
 
 
 if __name__ == "__main__":
